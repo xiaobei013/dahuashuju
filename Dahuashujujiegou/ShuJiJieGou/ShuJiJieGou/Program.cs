d64fc228ded18a5d369015f5f54a273b4900a577
zbc <ee516@126.com>
2021-12-23 23:22:14 +0800

    创建工程

---
+++ Dahuashujujiegou/ShuJiJieGou/ShuJiJieGou/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShuJiJieGou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
